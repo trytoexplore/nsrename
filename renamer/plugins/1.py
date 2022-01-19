c14b43269a5f4d2d8701b4592165ebf94674f24c
trytoexplore <97546125+trytoexplore@users.noreply.github.com>
2022-01-19 13:12:53 +0530

    Update 1.py

--- renamer/plugins/1.py
+++ renamer/plugins/1.py
@@ -19,7 +19,8 @@ async def force_sub(c, m):
         except UserNotParticipant:
             button = [[InlineKeyboardButton('join Updates channel', url=f'https://t.me/{Config.FORCE_SUB}')]]
             markup = InlineKeyboardMarkup(button)
-            return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
+            return await m.reply_text(text="Hey {m.from_user.mention(style='md')} you need join My updates channel in order to use me 😉\n\n"
+                "__Press the Following Button to join Now 👇__", parse_mode='markdown', reply_markup=markup, quote=True)
 
         except ChatAdminRequired:
             logger.warning(f"Make me admin in @{Config.FORCE_SUB}")
